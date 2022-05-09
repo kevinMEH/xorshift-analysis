f108c3b3270f4a708db0b684ffc166b40ba22ea6
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-05-08 21:06:12 -0400

    Simulate + IO to file

--- src/main.c
+++ src/main.c
@@ -1,19 +1,61 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
 #include <time.h>
 
 #include "main.h"
 #include "random.h"
 
+#define PATH "./test/"
+FILE* report;
+FILE* raw;
+
 int main() {
-    ShiftState state;
-    state.x[0] = 0b1101010000110100111010111101011000000111100010110110011101001100;
-    state.x[1] = 0b0110110111110000011111110110000110011010111101111111101111010000
+    report = fopen(PATH "report.txt", "w+");
+    raw = fopen(PATH "raw.txt", "w+");
+
+    ShiftState original;
+    original.x[0] = 0b1101010000110100111010111101011000000111100010110110011101001100;
+    original.x[1] = 0b0110110111110000011111110110000110011010111101111111101111010000
                     + (time(NULL) + 1) * (time(NULL) + 1) * time(NULL) * time(NULL);
+    ShiftState state;
+
+    memcpy(&state, &original, sizeof(state));
+    simulate10k(&xorshift, &state, report, raw);
     
-    for(int i = 0; i < 1000; i++) {
-        u64 num = xorshift(&state);
-        printf("%0.5lf\n", num / (0x8000000000000000u * 2.0));
-    }
+    fclose(report);
+    fclose(raw);
 
     return 0;
+}
+
+void simulate10k(RandomFunction function, ShiftState* state, FILE* report, FILE* raw) {
+    double numbers[10000];
+
+    double totalDist = 0.0;
+    double signedSquareDist = 0.0;
+
+    u64 first = function(state);
+    double last = first / (0x8000000000000000u * 2.0);
+    numbers[0] = last;
+    
+    for(int i = 1; i < 10000; i++) {
+        u64 num = function(state);
+        double normalized = num / (0x8000000000000000u * 2.0);
+        numbers[i] = normalized;
+        
+        totalDist += fabs(last - normalized);
+        signedSquareDist += fabs(last - normalized) * (last - normalized);
+        last = normalized;
+
+        fprintf(raw, "%0.20lf\n", normalized);
+    }
+    
+    double averageDist = totalDist / 9999;
+    
+    printf("%lf\n", averageDist);
+    printf("%lf\n", signedSquareDist);
+    
+    fprintf(report, "Average Distance Between Numbers: %lf\n", averageDist);
+    fprintf(report, "Signed Square Distance Between Numbers: %lf\n", signedSquareDist);
 }
--- src/main.h
+++ src/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include "random.h"
+
+typedef u64 (*RandomFunction)(ShiftState*);
+
 int main();
+void simulate10k(RandomFunction, ShiftState*, FILE*, FILE*);
 
 #endif
